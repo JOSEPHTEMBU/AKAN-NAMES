9bce1c216f9a239aef4bc53ec9113877a00db87b
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-08-23 23:07:45 +0300

    edit js in javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -20,6 +20,7 @@ var akanFemaleNames = [
 function getAkanName() {
 	var gender = document.getElementById("gender").value;
 	var dateOfBirth = document.getElementById("date").value;
+	var year = parseInt(dateOfBirth.substr(0, 5));
 	var DD = parseInt(dateOfBirth.substr(8, 9));
 	var CC = parseInt(dateOfBirth.substr(0, 2));
 	var MM = parseInt(dateOfBirth.substr(5, 6));
@@ -27,8 +28,10 @@ function getAkanName() {
 	var day = parseInt(
 		(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7
 	);
-
-	if (gender == "Female") {
+	var date = new Date(year, MM - 1, DD);
+	if (date > new Date()) {
+		alert("Invalid Date:Date has not yet reached");
+	} else if (gender == "Female") {
 		alert("Your Ghanaian name equivalent is " + akanFemaleNames[day]);
 	} else {
 		alert("Your Ghanaian name equivalent is " + akanMaleNames[day]);
